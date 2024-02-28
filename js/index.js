240fb2cfc252b25fbd30e23e5898b054ecaf713c
AmStanDem <96748673+AmStanDem@users.noreply.github.com>
2024-02-28 16:16:11 +0100

    a

--- js/index.js
+++ js/index.js
@@ -1,12 +1,42 @@
-const btnConvert = document.getElementById('btnConvert');
 
-btnConvert.addEventListener('click', () =>
-{
-    let number = Math.abs(document.getElementById('num').value);
+const lengthTypes={ //a record of every length type with values that they are relative to each other
+    Nm:1, //the smallest unit in the calculator, thus everything else is a COUNT of Nm
+    mm:1000, //one micrometre is 1000 nanometres
+    Mm:1e6, //one millimetre is 1 million nanometres
+    Cm:1e7, //one cm is 10 million nanometres
+    In:2.54e7, //one inch is 25.4 million nanometres
+    f:3.048e8, //one foot is 304.8 million nanometres
+    Y:9.144e8, //one yard is 914.4 million nanometres
+    m:1e9, //one metre is 1 billion nanometers
+    Km:1e12, //one kilometre is 1 trillion nanometres
+    Mi:1.609e12, //one mile is 1.609 trillion nanometres
+    NM:1.852e12, //one nautical mile is trillion nanometres
+}
 
-    let startMeasure = document.getElementById('startMeasure');
-    let valStartMeasure = startMeasure.options[startMeasure.selectedIndex].value;
 
-    let endMeasure = document.getElementById('endMeasure');
-    let valEndMeasure = endMeasure.options[endMeasure.selectedIndex].value;
-});
+//1 for the bar on the left, 2 for the bar on the right
+const input1 = document.getElementById("num");
+const inputType1 = document.getElementById("startMeasure");
+const input2 = document.getElementById("result");
+const inputType2 = document.getElementById("endMeasure");
+
+function handleChange(e){
+    let type1=inputType1.value, type2=inputType2.value
+    if([input1,inputType1,inputType2].includes(e.target||e.path[0])){
+        //left hand side changed by human(so set right side)
+        const n=parseInt(input1.value)
+        if(isNaN(n)){return null}
+        input2.value=n*(lengthTypes[type1]/lengthTypes[type2])
+    }
+    else{
+        //right hand side changed by human(so set left side)
+        const n=parseInt(input2.value)
+        if(isNaN(n)){return null}
+        input1.value=n*(lengthTypes[type2]/lengthTypes[type1])
+    }
+}
+
+input1.addEventListener('change',handleChange)
+input2.addEventListener('change',handleChange)
+inputType1.addEventListener('change',handleChange)
+inputType2.addEventListener('change',handleChange)
